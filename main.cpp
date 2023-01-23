2b82d0ade5d1fbd3d2272902e6b6c59e23b28dfa
Junwon Lee <cpprhtn@naver.com>
2023-01-23 20:22:27 +0900

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <thread>
+
+int main()
+{
+	// namenode 생성 
+	//     - secondary namenode는 나중에 구현
+	// datanode 생성
+	//     - block size = 128
+	// namenode에 연결
+	// thread detach
+	return 0;
+}
